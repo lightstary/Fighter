537bca2bf79fb839b8c3649f85fe83c8cbf3ba21
Thomas B. <111815848+moonshinetcb@users.noreply.github.com>
2025-11-16 21:58:29 -0500

    trying to fix ts

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -82,7 +82,7 @@ public class Player : MonoBehaviour
         transform.position = cam.ViewportToWorldPoint(vp);
 
     }
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Heart")
         {
