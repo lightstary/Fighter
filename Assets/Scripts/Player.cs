3017b17af7fa4065616ea60d74014299305a8d81
Thomas B. <111815848+moonshinetcb@users.noreply.github.com>
2025-11-16 19:42:51 -0500

    dawd

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -75,21 +75,5 @@ public class Player : MonoBehaviour
 
         // 6) Convert back to world space at the same depth
         transform.position = cam.ViewportToWorldPoint(vp);
-    
-    void OnTriggerEnter2D(Collider2D other)
-    {
-            if (other.tag == "Heart")
-            {
-                if (lives < 3)
-                {
-                    lives += 1;
-                }
-                else
-                {
-                    score += 1;
-                }
 
-                Destroy(other.gameObject);
-            }
-    }
 }
